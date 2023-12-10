76cf89e2b4ef5e846e69b523e8b1e7e6faea7984
xkritti <krittamet0404@gmail.com>
2023-12-11 06:04:38 +0700

    ✨ feat: ETHDiffCondition

---
+++ script/deploy/DeployETHDiffCondition.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "forge-std/Script.sol";
+import "../../src/ETHDiffCondition.sol";
+
+contract DeployETHDiffCondition is Script {
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Price Feeds For Avalanche FUJI Testnet
+        // ETH/USD => 0x86d67c3D38D2bCeE722E601025C25a575021c6EA
+
+        new ETHDiffCondition(0x86d67c3D38D2bCeE722E601025C25a575021c6EA);
+
+        vm.stopBroadcast();
+    }
+}
